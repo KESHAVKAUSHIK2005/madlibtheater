f6114189da0658f1517f1d0d264b1c6fba18ce7c
KESHAVKAUSHIK2005 <keshakaushik18rcb@gmail.com>
2020-12-20 18:24:34 +0530

    first commit

---
+++ mad lib theater - py.py
@@ -0,0 +1,22 @@
+import math
+
+proffession=input("enter proffession : ")
+place=input("enter place:")
+dish=input("enter dish :")
+adjective=input("enter adjective:")
+location=input("enter location:")
+
+
+
+
+
+
+
+
+
+print("hi, i am keshav.i am the" +  proffession + " of this hotel.")
+print("welcome to our" +  place + "what would you like to have?")
+print("i would like to have" +  dish )
+print("ohh!i also like that,its" + adjective )
+print("from where do you belong ? , i am from " + location )
+print("okay , bye!!, it was a nice meeting with you ")
